206c28273eee05bc1afdc0d324b053bd7bd42e25
yueyingyifeng <yueyingyifeng@Gmail.com>
2024-09-15 18:02:19 +0800

    启用 session

--- Program.cs
+++ Program.cs
@@ -17,13 +17,25 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(builder => builder.WithOrigins(urls).AllowAnyMethod().AllowAnyHeader().AllowCredentials())
 );
 
+//----------Session 启用
+// 添加 DataProtection 服务 (用于 session 加密)
+builder.Services.AddDataProtection();
+builder.Services.AddDistributedMemoryCache();
+// 添加 Session 服务
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Session 过期时间
+    options.Cookie.HttpOnly = true;                 // 确保 cookie 只能通过 HTTP 访问
+    options.Cookie.IsEssential = true;              // 标记为必要的 cookie
+});
+//----------------
 
 var app = builder.Build();
 
 
 app.UseCors();
 
-//----------------
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
